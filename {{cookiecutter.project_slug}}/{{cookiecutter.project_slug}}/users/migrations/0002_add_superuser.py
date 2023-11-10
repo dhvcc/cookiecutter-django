61f1d8f019e9df5e1d99c3afeb312dee3096cc50
dhvcc <alexey.artishevskiy@gmail.com>
2023-11-10 20:49:52 +0300

    Add super user migration (dev temp)

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/migrations/0002_add_superuser.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+from django.contrib.auth import get_user_model
+
+
+def create_superuser(apps, schema_editor):
+    User = get_user_model()
+    User.objects.create_superuser('u', '', 'u')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("users", "0001_initial"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
