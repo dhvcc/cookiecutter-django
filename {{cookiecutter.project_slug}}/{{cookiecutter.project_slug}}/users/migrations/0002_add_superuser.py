e7969b55d53cd9f9f4087fd196d1f2da3077bb84
Andrew Strongin <andrew.strongin@innowise-group.com>
2023-11-22 07:39:49 +0300

    Hotfix missing

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/migrations/0002_add_superuser.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/migrations/0002_add_superuser.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 
 def create_superuser(apps, schema_editor):
     User = get_user_model()
-    User.objects.create_superuser(username='u', email='u@u.com', password='u')
+    User.objects.create_superuser(email='u@u.com', password='u', username='u')
 
 
 class Migration(migrations.Migration):
