9379979b909e17a88420156205d517f2281bbdcf
Andrew Strongin <andrew.strongin@innowise-group.com>
2023-11-24 21:56:58 +0300

    Fix missing

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/migrations/0002_add_superuser.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/migrations/0002_add_superuser.py
@@ -4,10 +4,10 @@ from django.contrib.auth import get_user_model
 
 def create_superuser(apps, schema_editor):
     User = get_user_model()
-    {%- if cookiecutter.username_type == "email" %}
-    User.objects.create_superuser(email='u@u.com', password='u', name='u')
+    {%- if cookiecutter.username_type == "username" %}
+    User.objects.create_superuser(password='u', email='', username='u')
     {%- elif cookiecutter.username_type == "email" %}
-    User.objects.create_superuser(email='u@u.com', password='u', username='u')
+    User.objects.create_superuser(email='u@u.com', password='u', name='u')
     {%- endif %}
 
 
